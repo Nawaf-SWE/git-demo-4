553797352236ff53ff8c04cc922a93e4696fc020
Nawaf <125497395+Nawaf-SWE@users.noreply.github.com>
2023-03-01 23:11:12 +0300

    added sum

--- demo.py
+++ demo.py
@@ -3,6 +3,10 @@
 def square(x):
     return pow(x, 2)
 
+# @param x, y: number
+# @return x plus y
+def sum(x, y):
+    return x+y
 
 print("the square of 2 is", square(2))
 print("the square of 3 is", square(3))
